a79c360b6d3646033eede39085cf254b2e34105e
Franciscus Agnew <franciscusagnew@users.noreply.github.com>
2024-11-04 16:50:08 -0500

    Added and modified various files

--- src/restaurant/urls.py
+++ src/restaurant/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import sayHello
+# from .views import sayHello, index
+from . import views
 
 urlpatterns = [
     # http://127.0.0.1:8000/restaurant/
-    path('', sayHello, name='sayHello')
+    # path('', sayHello, name='sayHello')
+    path('', views.index, name='home')
 ]
